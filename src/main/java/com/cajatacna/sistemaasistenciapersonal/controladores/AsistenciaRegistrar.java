415f950fa478eba3c7535336b4470a039157f991
William Condori Quispe <william.condori@accenti.io>
2024-02-23 17:33:50 -0500

    Agregar controladore

---
+++ src/main/java/com/cajatacna/sistemaasistenciapersonal/controladores/AsistenciaRegistrar.java
@@ -0,0 +1,35 @@
+package com.cajatacna.sistemaasistenciapersonal.controladores;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet(name = "AsistenciaControlador", urlPatterns = { "/asistencia" })
+public class AsistenciaRegistrar extends HttpServlet {
+
+    private boolean verificarSesion(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        HttpSession session = request.getSession(false);
+        if (session == null || session.getAttribute("empleado") == null) {
+            request.setAttribute("error", "Inicia sesión para acceder a esta página");
+            response.sendRedirect(request.getContextPath() + "/login");
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        boolean estaAutenticado = this.verificarSesion(request, response);
+        if (estaAutenticado) {
+            RequestDispatcher dispatcher = request.getRequestDispatcher("/vistas/asistencia/AsistenciaRegistrar.jsp");
+            dispatcher.forward(request, response);
+        }
+    }
+}
